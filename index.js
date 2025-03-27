f3cfa610f4ea009694e1fdb1fd701fcfe4999398
OLAYEMI <awoyemiolayemi27@gmail.com>
2025-03-27 11:08:37 +0100

    Made changes

--- index.js
+++ index.js
@@ -12,13 +12,13 @@ app.use(express.json());
 app.use(cookieParse());
 connectDB()
 app.use(express.json())
-app.use(cors())
+// app.use(cors())
 
-// const corOpt = {
-//     origin: CLIENT_URL || ['http://localhost:3000'],
-//     credential: true,
-//     methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE'],
-// }
+const corOpt = {
+    origin: CLIENT_URL || ['http://localhost:5173'],
+    credentials: true,
+    methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE'],
+}
 
 // const corOpt = {
 //     origin: process.env.CLIENT_URL || ['http://localhost:3000'],
@@ -26,7 +26,7 @@ app.use(cors())
 // }
 
 
-// app.use(cors(corOpt))
+app.use(cors(corOpt))
 
 app.use('/api/v1', authROute)
 app.listen(PORT, ()=>console.log(`Server running on port ${PORT}`))
